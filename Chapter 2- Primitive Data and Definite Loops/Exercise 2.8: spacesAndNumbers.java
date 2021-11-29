2c4a30559d00cde3d32a9f057cbdb29943321017
Fcantwell <95243733+Fcantwell@users.noreply.github.com>
2021-11-29 12:57:31 -0500

    Update Exercise 2.8: spacesAndNumbers.java
    This is another way that 2.8 can be completed.

--- Chapter 2- Primitive Data and Definite Loops/Exercise 2.8: spacesAndNumbers.java
+++ Chapter 2- Primitive Data and Definite Loops/Exercise 2.8: spacesAndNumbers.java
@@ -7,10 +7,10 @@
 55555
 /*
 
-for(int i = 1; i <= 5; i++) {
-for(int j = 5 - i; j > 0; j--)
-System.out.print(" ");
-for(int j = 0; j < i; j++)
-System.out.print(i);
-System.out.println();
+for(int i = 1; i <= 5; i++){
+    for(int j = 4 - i; j >= 0; j--)
+        System.out.print(" ");
+    for(int j = 1; j <= i; j++)
+        System.out.print(i);
+        System.out.println();
 }
